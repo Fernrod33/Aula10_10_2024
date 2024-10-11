7e2f8b78b46b1327d8b9f1f807a038fe84979858
Fernrod33 <frodrigues1@minha.fag.edu.br>
2024-10-10 21:27:24 -0300

    Feito

---
+++ Atividade10_10_24/src/sistema/CadastroDeProdutos.java
@@ -0,0 +1,61 @@
+package sistema;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+import objetos.Produto;
+
+public class CadastroDeProdutos {
+	
+	static Scanner scan = new Scanner(System.in);
+	static ArrayList<Produto> lista = new ArrayList<Produto>();
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Bem vindo ao sistema!");
+		boolean continuar = true;
+		while (continuar) {
+			exibirMenu();
+			int escolha = scan.nextInt();
+			switch (escolha) {
+				case 1: {
+					cadastrarProduto();
+					break;
+				}
+				case 2: {
+					for (Produto novoProduto : lista) {
+						System.out.println(novoProduto.toString());
+					}
+					break;
+				}
+				case 3: {
+					continuar = false;
+					System.out.println("Saindo do sistema!");
+					return;
+				}
+				default: {
+					System.out.println("Opcao incorreta, tente novamente");
+				}
+			}
+		}
+	}
+	
+	static void cadastrarProduto() {
+		System.out.println("Digite o nome do produto:");
+		String nome = scan.next();
+		System.out.println("Digite o preco do produto:");
+		double preco = scan.nextDouble();
+		System.out.println("Digite a quantidade do produto:");
+		int quantidade = scan.nextInt();
+		Produto novoProduto = new Produto(nome, preco, quantidade);
+		lista.add(novoProduto);
+		System.out.println("Produto cadastrado com sucesso!");
+	}
+	
+	static void exibirMenu() {
+		System.out.println("Digite a opcao desejada:\n"
+				+ "1. Cadastrar novo produto.\n"
+				+ "2. Listar produtos cadastrados.\n"
+				+ "3. Sair do sistema.");
+	}
+	
+}
